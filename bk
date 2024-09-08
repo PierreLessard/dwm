c3ec1e73402d1e66a57b1925b2242205336af08c
PierreLessard <lessardpw@gmail.com>
2024-09-08 17:56:09 -0400

    cursor warp brave caveat

--- bk
+++ bk
@@ -48,7 +48,6 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int attachbelow = 1;    /* 1 means attach after the currently active window */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
-static const int focusonwheel = 0; /* Could break something added last second*/
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "T",      tile },    /* first entry is default */
--- dwm.c
+++ dwm.c
@@ -919,6 +919,8 @@ focusmon(const Arg *arg)
 	unfocus(selmon->sel, 0);
 	selmon = m;
 	focus(NULL);
+	if (selmon->sel)
+		XWarpPointer(dpy, None, selmon->sel->win, 0, 0, 0, 0, selmon->sel->w/2, selmon->sel->h/2);
 }
 
 void
@@ -944,6 +946,7 @@ focusstack(const Arg *arg)
 	if (c) {
 		focus(c);
 		restack(selmon);
+		XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, c->w/2, c->h/2);
 	}
 }
 
@@ -1195,6 +1198,8 @@ manage(Window w, XWindowAttributes *wa)
 	c->mon->sel = c;
 	arrange(c->mon);
 	XMapWindow(dpy, c->win);
+	if (c && c->mon == selmon)
+		XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, c->w/2, c->h/2);
 	focus(NULL);
 }
 
@@ -2247,6 +2252,9 @@ unmanage(Client *c, int destroyed)
 {
 	Monitor *m = c->mon;
 	XWindowChanges wc;
+	
+	XClassHint ch = { NULL, NULL };
+	int is_brave = XGetClassHint(dpy, c->win, &ch) && ch.res_class && (strcmp(ch.res_class, "Brave-browser") == 0);
 
 	detach(c);
 	detachstack(c);
@@ -2266,6 +2274,9 @@ unmanage(Client *c, int destroyed)
 	focus(NULL);
 	updateclientlist();
 	arrange(m);
+	if (m == selmon && m->sel && !is_brave)
+		XWarpPointer(dpy, None, m->sel->win, 0, 0, 0, 0,
+		             m->sel->w/2, m->sel->h/2);
 }
 
 void
